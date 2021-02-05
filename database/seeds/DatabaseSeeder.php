e4ff60e90ae664d2399f9476395c458b346afd32
lamvantan <vantan291@yahoo.com>
2021-02-06 01:53:04 +0700

    update seed2

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(DatabaseSeederUser::class);
+        $this->call(DatabaseSeederRoom::class);
     }
 }
 
@@ -27,6 +27,32 @@ class DatabaseSeederApp extends Seeder
         ]);
     }
 }
+// class DatabaseSeederRoom extends Seeder
+// {
+
+//     public function run()
+//     {
+//         DB::table('Phong')->insert([
+//             'name_room' => 'A1',
+//             'price_room' => 2000000,
+//             'area_room' => 20,
+//             'person_quantity' => 2,
+//             'detail_room' => 'Phong thoang mat',
+//             'status_room' => 1,
+//             'condition_room' => 0,
+//             'price_last' => 2000000,
+//             'chiSoDienBanDau' => 0,
+//             'chiSoNuocBanDau' => 0,
+//             'pinRooms' => 1,
+//             'ngayHetHanPin' => '2021-02-08 19:51:34',
+//             'MaTaiKhoan' => 1,
+//             'id_floor' =>1,
+//             'id_roomtype' => 1
+//         ]);
+//     }
+// }
+
+
 class DatabaseSeederUser extends Seeder
 {
 
@@ -39,6 +65,34 @@ class DatabaseSeederUser extends Seeder
             'ngayBatDau' => '2021-02-05 19:51:34',
             'ngayKetThuc' => '2021-03-05 19:51:34',
             'priceApp'  => 199000
+        ]);
+    }
+}
+
+
+class DatabaseSeederFloor extends Seeder
+{
+
+    public function run()
+    {
+        DB::table('Tang')->insert([
+            'name_floor' => 'Tang 1',
+            'status_floor' => 1,
+            'MaTaiKhoan' => 1
+        ]);
+    }
+}
+
+
+class DatabaseSeederRoomType extends Seeder
+{
+
+    public function run()
+    {
+        DB::table('LoaiPhong')->insert([
+            'name_roomtype' => 'Tang 1',
+            'status_roomtype' => 1,
+            'MaTaiKhoan' => 1
         ]);
     }
 }
