cd49d62585076bf3b720a74dc8f46720fe18e1cf
lamvantan <vantan291@yahoo.com>
2021-02-06 05:17:41 +0700

    commit

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(DatabaseSeederRoom::class);
+        $this->call(DatabaseSeederApp::class);
     }
 }
 
@@ -24,75 +24,6 @@ class DatabaseSeederApp extends Seeder
             'id_dongia' => '1',
             'trangthai' => '1',
             'giaTien' => 199000,
-        ]);
-    }
-}
-class DatabaseSeederRoom extends Seeder
-{
-
-    public function run()
-    {
-        DB::table('Phong')->insert([
-            'name_room' => 'A1',
-            'price_room' => 2000000,
-            'area_room' => 20,
-            'person_quantity' => 2,
-            'detail_room' => 'Phong thoang mat',
-            'status_room' => 1,
-            'condition_room' => 0,
-            'price_last' => 2000000,
-            'chiSoDienBanDau' => 0,
-            'chiSoNuocBanDau' => 0,
-            'pinRooms' => 1,
-            'ngayHetHanPin' => '2021-02-08 19:51:34',
-            'MaTaiKhoan' => 1,
-            'id_floor' =>1,
-            'id_roomtype' => 1
-        ]);
-    }
-}
-
-
-class DatabaseSeederUser extends Seeder
-{
-
-    public function run()
-    {
-        DB::table('users')->insert([
-            'email' => 'lamvantan03@gmail.com',
-            'password' => bcrypt('123123'),
-            'permission' => 1,
-            'ngayBatDau' => '2021-02-05 19:51:34',
-            'ngayKetThuc' => '2021-03-05 19:51:34',
-            'priceApp'  => 199000
-        ]);
-    }
-}
-
-
-class DatabaseSeederFloor extends Seeder
-{
-
-    public function run()
-    {
-        DB::table('Tang')->insert([
-            'name_floor' => 'Tang 1',
-            'status_floor' => 1,
-            'MaTaiKhoan' => 1
-        ]);
-    }
-}
-
-
-class DatabaseSeederRoomType extends Seeder
-{
-
-    public function run()
-    {
-        DB::table('LoaiPhong')->insert([
-            'name_roomtype' => 'Tang 1',
-            'status_roomtype' => 1,
-            'MaTaiKhoan' => 1
         ]);
     }
 }
